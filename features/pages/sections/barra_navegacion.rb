95cb95b226ea93fbf50aa3be275df02fd12e1915
root <easytico@gmail.com>
2016-08-02 17:31:59 -0600

    Ejemplo seccion 7 cucumber

--- features/pages/sections/barra_navegacion.rb
+++ features/pages/sections/barra_navegacion.rb
@@ -5,10 +5,20 @@ class BarraNavegacion < SitePrism::Section
 	element :siguiente, :xpath, ".//div[@class='wdform-page-and-images' and not (contains(@style,'display: none'))]//div[contains(@id,'page_next')]"
 	element :anterior, :xpath, ".//div[@class='wdform-page-and-images' and not (contains(@style,'display: none'))]//div[contains(@id,'page_previous')]"
 
+	# Funcionalidad: Este metodo se encarga de seleccionar el boton "siguiente".
+	# Parametros:
+	# => NA
+	# Retorno:
+	# => No retorna
 	def click_siguiente
 		siguiente.click
 	end
 
+	# Funcionalidad: Este metodo se encarga de seleccionar el boton "anterior".
+	# Parametros:
+	# => NA
+	# Retorno:
+	# => No retorna
 	def click_anterior
 		anterior.click
 	end
